4b2007da7b01e0aec12daec4fb98dc1c43e22d7e
root <root@DESKTOP-TOIFM3T.localdomain>
2026-01-27 05:42:31 +0000

    refactor docker compose with local/prod separation and frontend fixes

--- hostel-frontend-starter/docker-entrypoint.sh
+++ hostel-frontend-starter/docker-entrypoint.sh
@@ -5,6 +5,7 @@ set -e
 if [ -n "$REACT_APP_API_URL" ]; then
     echo "Setting API URL to: $REACT_APP_API_URL"
     find /usr/share/nginx/html -type f -name "*.js" -exec sed -i "s|http://localhost:8000|$REACT_APP_API_URL|g" {} \;
+    find /usr/share/nginx/html -type f -name "*.js" -exec sed -i "s|http://127.0.0.1:8000|$REACT_APP_API_URL|g" {} \;
 fi
 
 echo "Starting Nginx..."
